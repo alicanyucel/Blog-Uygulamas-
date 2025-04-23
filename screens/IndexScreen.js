2cc67f6242c5ede44c9f0e987c9d356b19cedb48
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-23 11:04:50 +0300

    first commit

---
+++ screens/IndexScreen.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useContext } from 'react';
+import BlogContext from '../context/BlogContext';
+
+export default function IndexScreen() {
+  const blogPosts = useContext(BlogContext);
+  return (
+    <View>
+      {/* <Text>IndexScreen </Text> */}
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
